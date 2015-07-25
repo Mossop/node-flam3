b9719291df455014eda33b57aa3b746053ef95f4
Dave Townsend <dtownsend@oxymoronical.com>
2015-07-25 12:58:01 -0700

    Remove old code.

--- src/fields.h
+++ src/fields.h
@@ -4,23 +4,7 @@
 #include <node.h>
 #include <nan.h>
 
-NAN_GETTER(DoublePropertyGetter);
-NAN_SETTER(DoublePropertySetter);
-NAN_GETTER(IntPropertyGetter);
-NAN_SETTER(IntPropertySetter);
 NAN_GETTER(ValuePropertyGetter);
-
-#define DEFINE_DBL_PROPERTY(name, field) \
-  NanObjectWrapHandle(this)->SetAccessor(NanNew<String>(#name), \
-    DoublePropertyGetter, DoublePropertySetter, NanNew<External>(&(field)), DEFAULT, DontDelete);
-
-#define DEFINE_STRUCT_DBL_PROPERTY(strct, field) DEFINE_DBL_PROPERTY(field, strct->field)
-
-#define DEFINE_INT_PROPERTY(name, field) \
-  NanObjectWrapHandle(this)->SetAccessor(NanNew<String>(#name), \
-    IntPropertyGetter, IntPropertySetter, NanNew<External>(&(field)), DEFAULT, DontDelete);
-
-#define DEFINE_STRUCT_INT_PROPERTY(strct, field) DEFINE_INT_PROPERTY(field, strct->field)
 
 #define DEFINE_READONLY_PROPERTY(name, value) \
   NanObjectWrapHandle(this)->SetAccessor(NanNew<String>(#name), \
